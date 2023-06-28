be9545d1f20736a767a34c18ca7790fc6f6a7e29
iPolarisu <elpolarisu@gmail.com>
2023-06-27 23:10:42 -0400

    chore: comment steam ladder scraper

--- steam_ladder_data/steam_ladder_scraper.py
+++ steam_ladder_data/steam_ladder_scraper.py
@@ -2,6 +2,9 @@ import requests
 import csv
 from bs4 import BeautifulSoup
 
+# this script web-scraps the top 250 latam users
+# with the largest friend list according to steamladder.com
+
 STEAM_LADDER_URL = 'https://steamladder.com/ladder/friends/'
 LATAM_CODES = ['AR', 'BR', 'CL', 'CO', 'CR', 'CU', 'DO', 'EC', 'GT', 'HN', 'MX', 'NI', 'PA', 'PE', 'PY', 'SV', 'UY', 'VE']
 HEADERS = ['steam_id', 'country']
@@ -31,5 +34,7 @@ with open(FILE_NAME, mode="w", newline="") as csv_file:
                 writer.writerow([steam_id, code])   
             except:
                 continue
-        
+
+print("Job done!")
+print(f"Check {FILE_NAME} for your results")
 
